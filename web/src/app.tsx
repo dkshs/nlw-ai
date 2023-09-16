7d5becec180a77fc149467fbbbb16ab0767e1407
DKSHS <88355080+dkshs@users.noreply.github.com>
2023-09-16 19:09:42 -0300

    class 3

---
+++ web/src/app.tsx
@@ -0,0 +1,134 @@
+import { useState } from "react";
+import { useCompletion } from "ai/react";
+
+import { Button } from "./components/ui/button";
+import { Label } from "./components/ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./components/ui/select";
+import { Separator } from "./components/ui/separator";
+import { Textarea } from "./components/ui/textarea";
+import { Slider } from "./components/ui/slider";
+import { VideoInputForm } from "./components/video-input-form";
+import { PromptSelect } from "./components/prompt-select";
+
+import { Github, Wand2 } from "lucide-react";
+
+export function App() {
+  const [temperature, setTemperature] = useState(0.5);
+  const [videoId, setVideoId] = useState<string | null>(null);
+
+  const { input, setInput, handleInputChange, handleSubmit, completion } =
+    useCompletion({
+      api: "http://localhost:3333/ai/complete",
+      body: {
+        videoId,
+        temperature,
+      },
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+  return (
+    <div className="flex min-h-screen flex-col">
+      <div className="flex items-center justify-between border-b px-6 py-3">
+        <h1 className="text-xl font-bold">upload.ia</h1>
+
+        <div className="flex items-center gap-3">
+          <span className="text-sm text-muted-foreground">
+            Desenvolvido com ❤️ no NLW da Rocketseat
+          </span>
+
+          <Separator orientation="vertical" className="h-6" />
+
+          <Button variant="outline">
+            <Github className="mr-2 h-4 w-4" />
+            Github
+          </Button>
+        </div>
+      </div>
+
+      <main className="flex flex-1 gap-6 p-6">
+        <div className="flex flex-1 flex-col gap-4">
+          <div className="grid flex-1 grid-rows-2 gap-4">
+            <Textarea
+              className="resize-none p-4 leading-relaxed "
+              placeholder="Inclua o prompt para a IA..."
+              value={input}
+              onChange={handleInputChange}
+            />
+            <Textarea
+              className="resize-none p-4 leading-relaxed "
+              placeholder="Resultado gerado pela IA..."
+              readOnly
+              value={completion}
+            />
+          </div>
+          <p className="text-sm text-muted-foreground">
+            Lembre-se: você pode utilizar a variável{" "}
+            <code className="text-violet-400">{`{transcription}`}</code> no seu
+            prompt para adicionar o conteúdo da transcrição do vídeo
+            selecionado.
+          </p>
+        </div>
+        <aside className="w-80 space-y-6">
+          <VideoInputForm onVideoUploaded={setVideoId} />
+
+          <Separator />
+
+          <form onSubmit={handleSubmit} className="space-y-6">
+            <div className="space-y-2">
+              <Label>Prompt</Label>
+              <PromptSelect onPromptSelected={setInput} />
+            </div>
+
+            <div className="space-y-2">
+              <Label>Modelo</Label>
+              <Select disabled defaultValue="gpt3.5">
+                <SelectTrigger>
+                  <SelectValue />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="gpt3.5">GPT 3.5-turbo 16k</SelectItem>
+                </SelectContent>
+              </Select>
+              <span className="block text-sm italic text-muted-foreground">
+                Você poderá customizar essa opção em breve
+              </span>
+            </div>
+
+            <Separator />
+
+            <div className="space-y-4">
+              <Label>Temperatura</Label>
+              <Slider
+                min={0}
+                max={1}
+                step={0.1}
+                className="cursor-grab active:cursor-grabbing"
+                value={[temperature]}
+                onValueChange={(value) => setTemperature(value[0])}
+              />
+              <span className="block text-sm italic leading-relaxed text-muted-foreground">
+                Valores mais altor tendem a deixar o resultado mais criativo e
+                com possíveis erros.
+              </span>
+            </div>
+
+            <Separator />
+
+            <Button type="submit" className="w-full">
+              Executar
+              <Wand2 className="ml-2 h-4 w-4" />
+            </Button>
+          </form>
+        </aside>
+      </main>
+    </div>
+  );
+}
